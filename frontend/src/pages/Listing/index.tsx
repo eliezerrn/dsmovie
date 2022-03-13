fbbbc7222776e5888331d60aa539e739faaa3eeb
Eliezer Roger Nardi <eliezerrn@gmail.com>
2022-03-13 14:55:21 -0300

    useState,useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,19 +1,28 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
 import { BASE_URL } from "utils/requests";
+import { MoviePage } from "types/movie";
 
 function Listing() {
 
-    //Forma errada
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+    const [pageNumber, setPageNumber] = useState(0);
+
+    useEffect(() =>{
+        axios.get(`${BASE_URL}/movies?size=12&page=0`)
         .then(response => {
-            console.log(response.data);
-
+            const data = response.data as MoviePage;
+            setPageNumber(data.number);
+            console.log(data);
         });
+    },[])
 
+    //Forma errada
+    
     return (
         <>
+            <p>{pageNumber}</p>
             <Pagination />
 
             <div className="container">
